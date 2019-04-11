6b10a9d839c55c66ab1235ad92318af97aff342d
leonardo lira <leon_lira_@hotmail.com>
2019-04-11 10:43:14 -0500

    cambio

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'https://testqval.admyo.com/server_qval/';
+
 //$config['base_url'] = 'http://localhost/serverqval/';
 /*
 |--------------------------------------------------------------------------
